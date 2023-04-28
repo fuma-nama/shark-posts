a5f940220d729a2241dcaaf9572f3e0a8aaebedf
SonMooSans <god63820869@gmail.com>
2023-04-28 20:58:40 +0800

    Feat: Draft new Posts via Dialog

---
+++ apps/nextjs/src/components/dialog/CreatePostDialog.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { Editor, EditorState } from "draft-js";
+
+import { trpc } from "~/utils/trpc";
+import { button } from "../system/button";
+import { Dialog } from "../system/dialog";
+
+export default function CreatePostDialog(props: {
+  open: boolean;
+  setOpen: (v: boolean) => void;
+}) {
+  const postMutation = trpc.post.create.useMutation({
+    onSuccess: () => {
+      props.setOpen(false);
+    },
+  });
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty(),
+  );
+
+  const onPost = () => {
+    postMutation.mutate({
+      content: editorState.getCurrentContent().getPlainText("\n"),
+    });
+  };
+
+  return (
+    <Dialog {...props}>
+      <Editor
+        editorState={editorState}
+        onChange={setEditorState}
+        placeholder="Write something here..."
+      />
+
+      <div className="mt-4 flex flex-row gap-3">
+        <button
+          disabled={postMutation.isLoading}
+          onClick={onPost}
+          className={button({
+            color: "primary",
+            content: "inline-flex",
+          })}
+        >
+          Post
+        </button>
+        <button
+          className={button({ color: "secondary" })}
+          onClick={() => props.setOpen(false)}
+        >
+          Close
+        </button>
+      </div>
+    </Dialog>
+  );
+}
