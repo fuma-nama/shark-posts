fb7c41258577c4c5e8bc2e8104dc6ff51766e7d4
SonMooSans <god63820869@gmail.com>
2023-04-28 20:55:16 +0800

    Fix: Error on create new posts

--- packages/api/src/router/post.ts
+++ packages/api/src/router/post.ts
@@ -1,25 +1,32 @@
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 
 export const postRouter = createTRPCRouter({
   all: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany({ orderBy: { id: "desc" } });
+    return ctx.prisma.post.findMany({
+      include: { author: true },
+      orderBy: { id: "desc" },
+    });
   }),
   byId: publicProcedure
     .input(z.object({ id: z.string() }))
     .query(({ ctx, input }) => {
       return ctx.prisma.post.findFirst({ where: { id: input.id } });
     }),
-  create: publicProcedure
+  create: protectedProcedure
     .input(
       z.object({
-        title: z.string().min(1),
         content: z.string().min(1),
       }),
     )
     .mutation(({ ctx, input }) => {
-      return ctx.prisma.post.create({ data: input });
+      return ctx.prisma.post.create({
+        data: {
+          ...input,
+          author_id: ctx.session.user.id,
+        },
+      });
     }),
   delete: publicProcedure.input(z.string()).mutation(({ ctx, input }) => {
     return ctx.prisma.post.delete({ where: { id: input } });
