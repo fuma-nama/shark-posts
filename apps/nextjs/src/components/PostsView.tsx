7fea0c1e2baf543ce5f2e3dff1f535259f719987
SonMooSans <god63820869@gmail.com>
2023-05-01 17:32:39 +0800

    Split Posts view components

---
+++ apps/nextjs/src/components/PostsView.tsx
@@ -0,0 +1,126 @@
+import { useEffect } from "react";
+import { useVirtualizer } from "@tanstack/react-virtual";
+import clsx from "clsx";
+
+import { type RouterOutputs } from "@acme/api";
+
+import { trpc } from "~/utils/trpc";
+import PostCard from "./PostCard";
+import { useViewport } from "./layouts/main";
+import { Spinner } from "./system/spinner";
+
+type PostsViewProps = {
+  allRows: RouterOutputs["post"]["get"];
+  isFetchingNextPage: boolean;
+  hasNextPage: boolean | undefined;
+  fetchLatestPage: () => void;
+  isFetchingLatestPage: boolean;
+  fetchNextPage: () => void;
+};
+
+export function PostsView({
+  hasNextPage,
+  allRows,
+  fetchNextPage,
+  isFetchingNextPage,
+  fetchLatestPage,
+  isFetchingLatestPage,
+}: PostsViewProps) {
+  const { rootRef } = useViewport();
+  const newPostsQuery = trpc.post.hasNewPosts.useQuery(
+    { after: allRows[0]?.timestamp },
+    {
+      initialData: { count: 0 },
+      enabled: allRows.length !== 0,
+      staleTime: 10 * 1000,
+      refetchInterval: 20 * 1000,
+    },
+  );
+
+  const virtualizer = useVirtualizer({
+    count: allRows.length + 1,
+    getScrollElement: () => rootRef.current,
+    estimateSize: () => 100,
+  });
+
+  const items = virtualizer.getVirtualItems();
+  const lastIndex = items[items.length - 1]?.index;
+
+  useEffect(() => {
+    if (lastIndex == null) {
+      return;
+    }
+
+    if (lastIndex >= allRows.length - 1 && hasNextPage && !isFetchingNextPage) {
+      void fetchNextPage();
+    }
+  }, [
+    hasNextPage,
+    fetchNextPage,
+    allRows.length,
+    isFetchingNextPage,
+    lastIndex,
+  ]);
+
+  return (
+    <div
+      className="relative"
+      style={{
+        height: `${virtualizer.getTotalSize()}px`,
+      }}
+    >
+      <div
+        className="absolute left-0 top-0 flex w-full max-w-screen-lg flex-col"
+        style={{
+          transform: `translateY(${items[0]?.start ?? 0}px)`,
+        }}
+      >
+        {items.map((virtualRow) => {
+          const post = allRows[virtualRow.index];
+
+          if (post == null) {
+            return (
+              <div
+                key={virtualRow.key}
+                data-index={virtualRow.index}
+                ref={virtualizer.measureElement}
+                className="flex flex-col items-center"
+              >
+                {hasNextPage === undefined || hasNextPage ? (
+                  <Spinner />
+                ) : (
+                  <p className="text-sm">Nothing more to load</p>
+                )}
+              </div>
+            );
+          }
+
+          return (
+            <div
+              key={virtualRow.key}
+              data-index={virtualRow.index}
+              ref={virtualizer.measureElement}
+            >
+              {newPostsQuery.isSuccess &&
+                newPostsQuery.data.count !== 0 &&
+                virtualRow.index === 0 && (
+                  <div
+                    className={clsx(
+                      "cursor-pointer rounded-md border-2 border-pink-400 bg-pink-500/10 p-2 text-center text-sm text-pink-300",
+                      "mb-3 hover:bg-pink-500/20",
+                    )}
+                    onClick={() => void fetchLatestPage()}
+                  >
+                    {isFetchingLatestPage
+                      ? "Loading New Posts..."
+                      : `Show ${newPostsQuery.data.count} Posts`}
+                  </div>
+                )}
+              <PostCard post={post} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
--- apps/nextjs/src/pages/home.tsx
+++ apps/nextjs/src/pages/home.tsx
@@ -1,15 +1,9 @@
-import { useEffect } from "react";
-import { useVirtualizer } from "@tanstack/react-virtual";
-import clsx from "clsx";
-
 import { trpc } from "~/utils/trpc";
-import PostCard from "~/components/PostCard";
-import { useMainLayout, useViewport } from "~/components/layouts/main";
-import { Spinner } from "~/components/system/spinner";
+import { PostsView } from "~/components/PostsView";
+import { useMainLayout } from "~/components/layouts/main";
 import type { NextPageWithLayout } from "./_app";
 
 const Home: NextPageWithLayout = () => {
-  const { rootRef } = useViewport();
   const {
     status,
     data,
@@ -44,106 +38,23 @@ const Home: NextPageWithLayout = () => {
 
   const allRows = data ? data.pages.flatMap((d) => d) : [];
 
-  const newPostsQuery = trpc.post.hasNewPosts.useQuery(
-    { after: allRows[0]?.timestamp },
-    {
-      initialData: { count: 0 },
-      enabled: allRows.length !== 0,
-      staleTime: 10 * 1000,
-      refetchInterval: 20 * 1000,
-    },
-  );
-
-  const virtualizer = useVirtualizer({
-    count: allRows.length + 1,
-    getScrollElement: () => rootRef.current,
-    estimateSize: () => 100,
-  });
-
-  const items = virtualizer.getVirtualItems();
-
-  useEffect(() => {
-    const lastItem = items[items.length - 1];
-
-    if (lastItem == null) {
-      return;
-    }
-
-    if (
-      lastItem.index >= allRows.length - 1 &&
-      hasNextPage &&
-      !isFetchingNextPage
-    ) {
-      void fetchNextPage();
-    }
-  }, [hasNextPage, fetchNextPage, allRows.length, isFetchingNextPage, items]);
-
   if (status === "error") {
-    return <span>Error: {error.message}</span>;
+    return (
+      <div className="flex h-full w-full flex-col items-center justify-center">
+        <p>Error: {error.message}</p>
+      </div>
+    );
   }
 
   return (
-    <div
-      className="relative"
-      style={{
-        height: `${virtualizer.getTotalSize()}px`,
-      }}
-    >
-      <div
-        className="absolute left-0 top-0 flex w-full max-w-screen-lg flex-col"
-        style={{
-          transform: `translateY(${items[0]?.start ?? 0}px)`,
-        }}
-      >
-        {items.map((virtualRow) => {
-          const post = allRows[virtualRow.index];
-
-          if (post == null) {
-            return (
-              <div
-                key={virtualRow.key}
-                data-index={virtualRow.index}
-                ref={virtualizer.measureElement}
-                className="flex flex-col items-center"
-              >
-                {hasNextPage === undefined || hasNextPage ? (
-                  <Spinner />
-                ) : (
-                  <p className="text-sm">Nothing more to load</p>
-                )}
-              </div>
-            );
-          }
-
-          return (
-            <div
-              key={virtualRow.key}
-              data-index={virtualRow.index}
-              ref={virtualizer.measureElement}
-            >
-              {newPostsQuery.isSuccess &&
-                newPostsQuery.data.count !== 0 &&
-                virtualRow.index === 0 && (
-                  <div
-                    className={clsx(
-                      "cursor-pointer rounded-md border-2 border-pink-400 bg-pink-500/10 p-2 text-center text-sm text-pink-300",
-                      "mb-3 hover:bg-pink-500/20",
-                    )}
-                    onClick={() => {
-                      void fetchPreviousPage();
-                    }}
-                  >
-                    {isFetchingPreviousPage
-                      ? "Loading New Posts..."
-                      : `Show ${newPostsQuery.data.count} Posts`}
-                  </div>
-                )}
-              <PostCard post={post} />
-            </div>
-          );
-        })}
-      </div>
-    </div>
+    <PostsView
+      allRows={allRows}
+      fetchLatestPage={() => void fetchPreviousPage()}
+      fetchNextPage={() => void fetchNextPage()}
+      hasNextPage={hasNextPage}
+      isFetchingLatestPage={isFetchingPreviousPage}
+      isFetchingNextPage={isFetchingNextPage}
+    />
   );
 };
 
